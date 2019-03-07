8d5223bdf2b3e0c0464bfe1e3c7494936a8f56e3
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-03-06 21:57:43 -0600

    login y registro de usuarios

--- routes/web.php
+++ routes/web.php
@@ -15,29 +15,9 @@ use App\Image;
 
 Route::get('/', function () {
 
-    // sacar todass las img
-    $images = Image::all();
-
-    foreach ($images as $image ) {
-        
-        echo $image->image_path . '<br>';
-        echo $image->user->name .' '. $image->user->surname . '<br>';
-
-        if(count($image->comments) >= 1){
-
-            echo '<h3>comentarios:</h3>';
-            foreach ($image->comments as $coment) {
-                # code...
-                echo $coment->user->name .' '. $coment->user->surname. ':';
-                echo $coment->content . '<br>'; 
-            }
-        }
-
-        echo 'Likes: ' . count($image->likes);
-
-        echo '<hr>';
-    }
-    die();
-
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
