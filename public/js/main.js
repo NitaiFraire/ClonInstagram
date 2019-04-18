d68f61d0325a73e352ad7de9b908787a30e491a3
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-04-17 22:00:36 -0500

    function para cambiar de color el corazon de like

---
+++ public/js/main.js
@@ -0,0 +1,68 @@
+var url = 'http://localhost/Actividades/masterPhp/clonInstagram/public/';
+
+window.addEventListener("load", function(){
+
+   // boton like
+   function like() {
+       
+       $('.btn-like').unbind('click').click(function(){
+    
+            $(this).addClass('btn-dislike').removeClass('btn-like');
+            $(this).attr('src', url + 'img/heartR.png');
+
+            $.ajax({
+
+                url: url + 'like/' + $(this).data('id'),
+                type: 'GET',
+                success: function(response){
+
+                    if(response.like){
+
+                        console.log('has dado like a la publicación');
+
+                    }else{
+
+                        console.log('error');
+                    }
+                }
+            });
+
+            dislike();
+       });
+   }
+   
+   like();
+
+    // boton dislike
+    function dislike() {
+        
+        $('.btn-dislike').unbind('click').click(function(){
+    
+            $(this).addClass('btn-like').removeClass('btn-dislike');
+            $(this).attr('src', url + 'img/heart.png');
+
+            $.ajax({
+
+                url: url + 'disLike/' + $(this).data('id'),
+                type: 'GET',
+                success: function(response){
+
+                    if(response.like){
+
+                        console.log('has dado dislike a la publicación');
+
+                    }else{
+
+                        console.log('error');
+                    }
+                }
+            });
+
+            like();
+       });
+    }
+
+    dislike();
+
+
+});
