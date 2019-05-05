998d29d651846cb6f1528a084cad76838d2e5bf4
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-05-05 17:10:59 -0500

    formulario para editar imagen

--- resources/views/image/edit.blade.php
+++ resources/views/image/edit.blade.php
@@ -7,17 +7,21 @@
                 <div class="card">
                     <div class="card-header">Editar magen</div>
                     <div class="card-body">
-                        <form action="#" method="post" enctype="multipart/form-data">
+                        <form action="{{ route('image.update') }}" method="post" enctype="multipart/form-data">
                             @csrf
+                            <input type="hidden" name="image_id" value="{{ $image->id}}">
                             <div class="form-group row">
                                 <label for="image_path" class="col-md-4 col-form-label text-md-right">Imagen</label>
                                 <div class="col-md-6">
                                     @if($image->user->image)
-                                        <div class="container-avatar mx-2">
-                                            <img class="avatar" src="{{ route('user.avatar', ['fileName' => $image->user->image]) }}" alt="">
-                                        </div>
+                                    <div class="container-avatar mx-2">
+                                            <img class="img-fluid mx-auto d-block avatar" src="{{ route('image.file', ['fileName' => $image->image_path])}}" alt="">
+                                    </div>
                                     @endif
-
+                                    <input type="file" name="image_path" id="image_path" class="form-control p-0 px-3" >
+                                    @if($errors->has('image_path'))
+                                        <span class="invalid-feedback" role="alert"><strong>{{ $errors->first('image_path') }}</strong></span>
+                                    @endif
                                 </div>
                             </div>
                             <div class="form-group row">
